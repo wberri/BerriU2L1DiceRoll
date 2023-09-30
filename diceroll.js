664d0900d8531e1f5ec64293f06ecf0b203b3925
wberri <114095020+wberri@users.noreply.github.com>
2023-09-30 14:53:44 -0400

    Add files via upload

---
+++ diceroll.js
@@ -0,0 +1,56 @@
+const button = document.querySelector('button');
+button.addEventListener("click", updateNums);
+
+function updateNums(){
+    const num1 = Math.floor(Math.random()*6 +1);
+    const num2 = Math.floor(Math.random()*6 +1);
+    switchImg1(num1);
+    switchImg2(num2);
+    const totalNum = document.querySelector("p");
+    const numericalVal = num1+num2;
+    totalNum.textContent = "Total: " + numericalVal;
+}
+
+function switchImg1(num1){
+    var pic = document.getElementById(1);
+    if (num1 ==1){
+        pic.src = "1side.png";
+    }
+    if (num1 ==2){
+        pic.src = "2side.png";
+    }
+    if (num1 ==3){
+        pic.src = "3side.png";
+    }
+    if (num1 ==4){
+        pic.src = "4side.png";
+    }
+    if (num1 ==5){
+        pic.src = "5side.png";
+    }
+    if (num1 ==6){
+        pic.src = "6side.png";
+    }
+}
+
+function switchImg2(num2){
+    var pic = document.getElementById(2);
+    if (num2 ==1){
+        pic.src = "1side.png";
+    }
+    if (num2 ==2){
+        pic.src = "2side.png";
+    }
+    if (num2 ==3){
+        pic.src = "3side.png";
+    }
+    if (num2 ==4){
+        pic.src = "4side.png";
+    }
+    if (num2 ==5){
+        pic.src = "5side.png";
+    }
+    if (num2 ==6){
+        pic.src = "6side.png";
+    }
+}
